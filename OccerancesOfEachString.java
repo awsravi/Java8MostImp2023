d8a888b52b778f71c107d981df992f023841c460
Ravikumar <127386201+awsravi@users.noreply.github.com>
2023-09-04 22:46:54 +0530

    Update and rename OccerancesOfEachCharacter.java to OccerancesOfEachString.java

--- OccerancesOfEachString.java
+++ OccerancesOfEachString.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class OccerancesOfEachCharacter {
+public class OccerancesOfEachString {
 	public static void main(String[] args) {
 
 		String str = "success";
