60c268a9a97b93324560d7a006cb17776302ba48
Ravikumar <127386201+awsravi@users.noreply.github.com>
2023-09-08 10:21:28 +0530

    OccereVowels
    OccereVowels

---
+++ OcceVowelsCount.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class OcceVowelsCount {
+	public static void main(String[] args) {
+		String str = "ravikumar";
+		
+		List<String> lstr = Arrays.asList(str.split(""));
+		Map<String, Long> occe = lstr.stream()
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		System.out.println(occe);
+
+		Map<Character, Long> vowelocce = str.chars().filter(ch -> "aeiou".indexOf(ch) != -1).mapToObj(ch -> (char) ch)
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		System.out.println(vowelocce);
+		
+	}
+}
